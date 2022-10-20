ba0ce92b6c370bc64e58c97d77a8f7b874c7d80f
Gaurav <gmchopra@wisc.edu>
2022-10-20 02:30:00 +0000

    Added Ibook and IBookMapper interfaces used by the DataWrangler

---
+++ IGameLoader.java
@@ -0,0 +1,18 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+
+/**
+ * Instances of this interface can be used to load book data from a XML file.
+ */
+public interface IGameLoader {
+   
+    /**
+     * This method loads the list of games from a XML file.
+     * @param filepathToXML path to the XML file relative to the executable
+     * @return a list of game objects
+     * @throws FileNotFoundException
+     */
+    List<IGame> loadGames(String filepathToXML) throws FileNotFoundException;
+
+}
+
