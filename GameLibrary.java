db0ece0435abeab53d0fd6eec3c520ac37c620ba
Ishan Bhutoria <ibhutoria@wisc.edu>
2022-11-16 05:41:15 +0000

    Added the GameLibrary

---
+++ GameLibrary.java
@@ -0,0 +1,22 @@
+import java.io.FileNotFoundException;
+
+public class GameLibrary {
+
+	public static void main(String[] args) throws FileNotFoundException{
+		// TODO Auto-generated method stub
+		IGameLoader gameLoader = new GameLoader();
+        // load the books from the data file
+        List<IGame> gameList = gameLoader.loadGames("vgsales.xml");
+        // instantiate the backend
+        IGameLibraryBackend backend = new GameLibraryBackend();
+        // add all the books to the backend
+        for (IGame game : gameList) backend.addGame(game);
+        // instantiate the scanner for user input
+        Scanner userInputScanner = new Scanner(System.in);
+        // instantiate the front end and pass references to the scanner, backend, and isbn validator to it
+        IGameFrontEnd frontend = new GameFrontEnd(userInputScanner, backend);
+        // start the input loop of the front end
+        frontend.runCommandLoop();
+	}
+
+}
